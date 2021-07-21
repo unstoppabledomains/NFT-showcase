7a9b8307602d2bf3f2d588adea07e21a833179d3
sudoryan <rleinbox@gmail.com>
2021-07-21 12:08:17 -0500

    Filter out UNSRegistry too

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,5 +1,6 @@
 import { Nft, OpenSeaAsset } from "../types";
-const UnstoppableContract = "0xd1e5b0ff1287aa9f9a268759062e4ab08b9dacbe";
+const CNSRegistry = "0xd1e5b0ff1287aa9f9a268759062e4ab08b9dacbe";
+const UNSRegistry = "0x049aba7510f45ba5b64ea9e658e342f904db358d";
 const ENSContract = "0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85";
 
 export const getOwner = async (domain: string) => {
@@ -39,7 +40,8 @@ export const getNfts = async (
           if (
             (c.name || c.token_id) &&
             c.image_url &&
-            c.asset_contract.address.toLowerCase() !== UnstoppableContract &&
+            c.asset_contract.address.toLowerCase() !== CNSRegistry &&
+            c.asset_contract.address.toLowerCase() !== UNSRegistry &&
             c.asset_contract.address.toLowerCase() !== ENSContract
           ) {
             const nft: Nft = {
